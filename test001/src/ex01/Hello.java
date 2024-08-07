23ec834fcd508d68f8d57d36848eb44e021c5e06
hn0856 <hanna0856@naver.com>
2024-08-07 11:02:12 +0900

    hi2 추가

--- test001/src/ex01/Hello.java
+++ test001/src/ex01/Hello.java
@@ -5,14 +5,9 @@ public class Hello {
 	public static void main(String[] args) {
 
 	System.out.println("Hello");
-<<<<<<< HEAD
-	System.out.println("Hello2");
-
-=======
 	System.out.println("Hi2");
 	System.out.println("Hi3");	
 
->>>>>>> branch 'master' of https://github.com/hn0856/test001.git
 	}
 
 }
