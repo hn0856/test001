e5ae4f89cb42d539601e5254c2922e0fe7fa73fb
hn0856 <hanna0856@naver.com>
2024-08-07 10:48:39 +0900

    Hello 3 추가

--- test001/src/ex01/Hello.java
+++ test001/src/ex01/Hello.java
@@ -3,9 +3,9 @@ package ex01;
 public class Hello {
 
 	public static void main(String[] args) {
-	System.out.println("Hello");
-	System.out.println("Hello2");
-	
+		System.out.println("Hello");
+		System.out.println("Hello2");
+		System.out.println("Hello3");
 
 	}
 
